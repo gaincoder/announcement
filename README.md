eb19e9b654a8510dc97d83979bec48001e84a6ee
Tim Moritz <tim_moritz@cds-service.com>
2021-12-14 08:52:36 +0100

    first version

---
+++ README.md
@@ -0,0 +1,11 @@
+announcement
+================================
+Announcement Plugin for the [**Bludit CMS**](https://www.bludit.com/)
+
+This plugin shows an announcement banner on the top of each page. 
+
+You can set start and end date/time , border color and html content.
+
+License
+-------
+This is open source software licensed under the [MIT license](https://tldrlegal.com/license/mit-license).
---
+++ plugin.php
@@ -0,0 +1,64 @@
+<?php
+
+class pluginAnnouncement extends Plugin {
+
+	public function init()
+	{
+		$this->dbFields = array(
+			'start'=>'1970-01-01 00:00:00',
+            'end' => '2042-12-31 23:59:59',
+            'text' => '',
+            'bordercolor' => 'darkblue'
+		);
+	}
+
+	public function form()
+	{
+		global $L;
+
+		$html  = '<div class="alert alert-primary" role="alert">';
+		$html .= $this->description();
+		$html .= '</div>';
+
+		$html .= '<div>';
+		$html .= '<label>'.$L->get('Start').'</label>';
+		$html .= '<input name="label" type="text" value="'.$this->getValue('start').'">';
+        $html .= '<label>'.$L->get('End').'</label>';
+        $html .= '<input name="end" type="text" value="'.$this->getValue('end').'">';
+        $html .= '<label>'.$L->get('Bordercolor').'</label>';
+        $html .= '<input name="bordercolor" type="text" value="'.$this->getValue('bordercolor').'">';
+        $html .= '<label>'.$L->get('Text').'</label>';
+        $html .= '<textarea name="text">'.$this->getValue('text').'</textarea>';
+        $html .= '<span class="tip">'.$L->get('HTML allowed').'</span>';
+		$html .= '</div>';
+
+		return $html;
+	}
+
+	public function siteHead()
+    {
+        $html = '';
+        if ($this->shouldBeVisible()) {
+
+            $html .= '<style media="screen">
+                           .announcement{
+                                position: relative;
+                                border: 1px '.$this->getValue('bordercolor').' solid;
+                                margin: 2px;
+                                padding: 2px;
+                                text-align: center;
+                           }
+                       </style>';
+
+            $html .= '<div class="announcement">' . $this->getValue('text',false) . '</div>';
+        }
+		return $html;
+	}
+
+    private function shouldBeVisible(){
+        $filled = strlen($this->getValue('text')) > 0;
+        $startPassed = time() >= strtotime($this->getValue('start'));
+        $endNotPassed = time() <= strtotime($this->getValue('end'));
+        return $filled && $startPassed && $endNotPassed;
+    }
+}
